a76ed8bd95268a258453b7eff217f93403bcab5d
fortune710 <fortune0208@yahoo.com>
2023-04-13 13:15:17 +0100

    mock data

---
+++ mock/presciption.ts
@@ -0,0 +1,122 @@
+export const PrescriptionData = [
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+    {
+        id: 1,
+        drug: {
+            name: "Drug 1"
+        },
+        patient: {
+            name: "Patient 1"
+        },
+        prescribed_dosage: 200
+    },
+] as const
