efe9b718a448d953dbcba9e0fbd67f8e404c5ba0
fortune710 <fortune0208@yahoo.com>
2023-05-03 22:54:35 +0100

    test data

---
+++ mock/users.ts
@@ -0,0 +1,22 @@
+export const Users = [
+    {
+        id: 0,
+        name: "James"
+    },
+    {
+        id: 1,
+        name: "Fortune"
+    },
+    {
+        id: 2,
+        name: "Kate"
+    },
+    {
+        id: 3,
+        name: "Micah"
+    },
+    {
+        id: 4,
+        name: "David"
+    }
+]
