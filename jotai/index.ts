065024eb6187c01aa5c6db90add150db1a8020ac
fortune710 <fortune0208@yahoo.com>
2023-06-05 10:00:26 +0100

    jotai store

---
+++ jotai/index.ts
@@ -0,0 +1,17 @@
+import { Prescriber } from "@/types";
+import { atom } from "jotai";
+
+export const currentUserAtom = atom<Prescriber>({
+    id: 0,
+    name: "",
+    email: "",
+    gender: "M",
+    verified: false,
+    profile_pic: "",
+    home_address: "",
+    license_expiry: 0,
+    pharmacy_id: 0,
+    delegates: [],
+    issued_prescriptions: [],
+    
+})
